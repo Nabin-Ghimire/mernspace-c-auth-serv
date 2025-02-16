43d590bac12e85b9476297016dafa7181a3d041e
Nabin Ghimire <129270093+Nabin-Ghimire@users.noreply.github.com>
2025-02-16 16:34:42 +0545

    test husky 2

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
 function welcome(name: string) {
     console.log(`Welcome ${name}`);
+    const user = {
+        name: 'Nabin',
+    };
+    const fname = user.name;
+    return name + fname;
 }
 welcome('Nabin');
