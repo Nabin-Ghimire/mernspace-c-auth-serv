204f4be05dbe367f9c8c915ca55ae9eef5b2cdcd
Nabin Ghimire <ghimiren2057@gmail.com>
2025-03-01 21:43:10 +0545

    add email sanitization

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -202,4 +202,26 @@ describe('POST /auth/register', () => {
             expect(users).toHaveLength(0);
         });
     });
+
+    describe('Fields are not in proper order', () => {
+        it('should trim the email field', async () => {
+            const userData = {
+                firstName: 'Nabin',
+                lastName: 'Shrestha',
+                email: '  ghimiren2057@gmail.com  ',
+                password: 'secret',
+            };
+
+            //Act
+
+            await request(app).post('/auth/register').send(userData);
+
+            //Assert
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+            const user = users[0];
+
+            expect(user.email).toBe('ghimiren2057@gmail.com');
+        });
+    });
 });
