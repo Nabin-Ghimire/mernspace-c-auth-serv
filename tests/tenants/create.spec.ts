61e21922733519f5e884af9b2801c682b276dc26
Nabin Ghimire <ghimiren2057@gmail.com>
2025-03-18 17:00:10 +0545

    add mistakenly left create tenant endpoint portion

--- tests/tenants/create.spec.ts
+++ tests/tenants/create.spec.ts
@@ -2,6 +2,7 @@ import { DataSource } from 'typeorm';
 import { AppDataSource } from '../../src/config/data-source';
 import request from 'supertest';
 import app from '../../src/app';
+import { Tenant } from '../../src/entity/Tenant';
 
 describe('POST /tenants', () => {
     let connection: DataSource;
@@ -31,6 +32,22 @@ describe('POST /tenants', () => {
                 .send(tenentData);
 
             expect(response.statusCode).toBe(201);
+        });
+
+        it('should create tenant in the database', async () => {
+            const tenentData = {
+                name: 'Tenant name',
+                address: 'Tenant address',
+            };
+
+            await request(app).post('/tenants').send(tenentData);
+
+            const tenantRepository = connection.getRepository(Tenant);
+            const tenants = await tenantRepository.find();
+
+            expect(tenants).toHaveLength(1);
+            expect(tenants[0].name).toBe(tenentData.name);
+            expect(tenants[0].address).toBe(tenentData.address);
         });
     });
 });
