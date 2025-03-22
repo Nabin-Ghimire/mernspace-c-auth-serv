ac8f4bceb0d0b032f6ab30e73d99a88852e69a8b
Nabin Ghimire <ghimiren2057@gmail.com>
2025-03-22 20:11:39 +0545

    add other user crud operations

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,13 +1,21 @@
 import { Repository } from 'typeorm';
 import { User } from '../entity/User';
 import bcrypt from 'bcryptjs';
-import { UserData } from '../types';
+import { LimitedUserData, UserData } from '../types';
 import createHttpError from 'http-errors';
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
 
-    async create({ firstName, lastName, email, password, role }: UserData) {
+    async create({
+        firstName,
+        lastName,
+        email,
+        password,
+        tenantId,
+        role,
+    }: UserData) {
+        type tenantId = number | string;
         const user = await this.userRepository.findOne({ where: { email } });
         if (user) {
             const err = createHttpError(400, 'Email already exists!');
@@ -24,6 +32,7 @@ export class UserService {
                 email,
                 password: hashedPassword,
                 role,
+                tenantId: tenantId ? { id: tenantId } : undefined,
             });
 
             return userData;
@@ -42,7 +51,51 @@ export class UserService {
         return await this.userRepository.findOne({ where: { email } });
     }
 
+    async findByEmailWithPassword(email: string) {
+        return await this.userRepository.findOne({
+            where: {
+                email,
+            },
+            select: [
+                'id',
+                'firstName',
+                'lastName',
+                'email',
+                'role',
+                'password',
+            ],
+        });
+    }
+
     async findById(id: number) {
         return await this.userRepository.findOne({ where: { id } });
+    }
+
+    async update(
+        userId: number,
+        { firstName, lastName, role }: LimitedUserData,
+    ) {
+        try {
+            return await this.userRepository.update(userId, {
+                firstName,
+                lastName,
+                role,
+            });
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (err) {
+            const error = createHttpError(
+                500,
+                'Failed to update the user in the database',
+            );
+            throw error;
+        }
+    }
+
+    async getAll() {
+        return await this.userRepository.find();
+    }
+
+    async deleteById(userId: number) {
+        return await this.userRepository.delete(userId);
     }
 }
