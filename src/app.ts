47a8630732af4466106895821af2810859257d68
Nabin Ghimire <ghimiren2057@gmail.com>
2025-05-02 12:50:56 +0545

    use error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import 'reflect-metadata';
-import logger from './config/logger';
-import { HttpError } from 'http-errors';
+
 import authRouter from './routes/auth';
 import tenantRouter from './routes/tenant';
 import userRouter from './routes/user';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import { globalErrorHandler } from './middlewares/globalErrorHandler';
 
 const app = express();
 
@@ -28,13 +28,6 @@ app.use('/auth', authRouter);
 app.use('/tenants', tenantRouter);
 app.use('/users', userRouter);
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-    logger.error(err.message);
-    const statusCode = err.statusCode || err.status || 500;
+app.use(globalErrorHandler);
 
-    res.status(statusCode).json({
-        errors: [{ type: err.name, msg: err.message, path: '', location: '' }],
-    });
-});
 export default app;
